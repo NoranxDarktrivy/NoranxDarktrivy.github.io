a7d8669a881eaedb45b101ace55b9bfec63376de
NoranxDarktrivy <152438476+NoranxDarktrivy@users.noreply.github.com>
2026-05-05 22:42:20 -0700

    Add files via upload

---
+++ src/data/talents/hunter/index.ts
@@ -0,0 +1,7 @@
+import beastMastery from './beast-mastery'
+import marksmanship from './marksmanship'
+import survival from './survival'
+
+const hunter = { class: 'hunter', specs: [beastMastery, marksmanship, survival] }
+
+export default hunter
