3864f56ef68e3f0735b93d6c8b97c8e4e6ea7a26
NoranxDarktrivy <152438476+NoranxDarktrivy@users.noreply.github.com>
2026-05-05 22:43:16 -0700

    Add files via upload

---
+++ src/data/talents/shaman/index.ts
@@ -0,0 +1,7 @@
+import elemental from './elemental'
+import enhancement from './enhancement'
+import restoration from './restoration'
+
+const shaman = { class: 'shaman', specs: [elemental, enhancement, restoration] }
+
+export default shaman
